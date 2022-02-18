9857118b6a8a022f34496757dcc598069b58315e
Arvind Kumar Suroju <74810455+ArvindKumarSuroju@users.noreply.github.com>
2022-02-17 16:05:56 -0800

    add login, signup and homepage

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
-const allPages = document.querySelectorAll('div.page');
-allPages[0].style.display = 'block';
+// const allPages = document.querySelectorAll('div.page');
+// allPages[0].style.display = 'block';
 
-function navigateToPage(event) {
-  const pageId = location.hash ? location.hash : '#login';
-  for (let page of allPages) {
-    if (pageId === '#' + page.id) {
-      page.style.display = 'block';
-    } else {
-      page.style.display = 'none';
-    }
-  }
-  return;
-}
-navigateToPage();
+// function navigateToPage(event) {
+//   const pageId = location.hash ? location.hash : '#login';
+//   for (let page of allPages) {
+//     if (pageId === '#' + page.id) {
+//       page.style.display = 'block';
+//     } else {
+//       page.style.display = 'none';
+//     }
+//   }
+//   return;
+// }
+// navigateToPage();
 
-//init handler for hash navigation
-window.addEventListener('hashchange', navigateToPage);
+// //init handler for hash navigation
+// window.addEventListener('hashchange', navigateToPage);
---
+++ script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+//setting up the Router with pages
+Router.init('mainArea', [
+    new Page('#on-boarding-customer', 'pages/on-boarding.html'),
+    new Page('#login-customer', 'pages/login-customer.html'),
+    new Page('#homepage', 'pages/homepage.html'),
+    new Page('#signup-customer', 'pages/signup-customer.html'),
+    new Page('#registration-successful', 'pages/registration-successful.html'),
+    new Page('#partner-login', 'pages/partner-login.html'),
+    new Page('#partner-signup', 'pages/partner-signup.html'),
+
+]);
